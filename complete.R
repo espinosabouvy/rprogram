245e8fb75d7edfb82ba81dfb03e5ad9c7cf78acf
LUIS ESPINOSA <espinosabouvy@gmail.com>
2016-09-15 21:44:43 -0500

    Curso Rprogramming backup completo

--- complete.R
+++
@@ -1,7 +0,0 @@
-complete <- function(direc, id = 1:332){
-     archivos <- list.files(direc,full.names = TRUE)
-     for (i in id) {
-          casos <- sum(complete.cases(read.csv(archivos[i])))
-          #falta crear el frame con los datos
-     }
-}
